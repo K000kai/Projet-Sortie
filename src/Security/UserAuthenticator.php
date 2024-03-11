d7c885cfcd29fd06a4dd79b756b1baa096798b42
K000kai <156654837+K000kai@users.noreply.github.com>
2024-03-12 00:20:14 +0100

    création de user fixtures et ajout de la balise script de bootstrap

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -34,7 +34,9 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
 
         return new Passport(
-            new UserBadge($email,fn(string $identifier)=>$this->userRepository->findUserByEmailOrUsername($identifier)),
+            new UserBadge($email),
+            //todo: à décommenter quand l'association profil et user sera faites
+            //new UserBadge($email,fn(string $identifier)=>$this->userRepository->findUserByEmailOrUsername($identifier)),
             new PasswordCredentials($request->request->get('password', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
