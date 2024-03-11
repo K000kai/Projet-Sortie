6abbf39fac08fa5b183bb3f8f0692ed7ee30b1e5
OGENDT <olivia_gendt@outlook.fr>
2024-03-11 15:31:09 +0100

    Création des entités Sortie, État, Lieu, Ville, Campus

---
+++ src/Entity/Outing.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OutingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OutingRepository::class)]
+class Outing
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateTimeStart = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registrationDeadline = null;
+
+    #[ORM\Column]
+    private ?int $nbRegistrationMax = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $infoOuting = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateTimeStart(): ?\DateTimeInterface
+    {
+        return $this->dateTimeStart;
+    }
+
+    public function setDateTimeStart(\DateTimeInterface $dateTimeStart): static
+    {
+        $this->dateTimeStart = $dateTimeStart;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationDeadline(): ?\DateTimeInterface
+    {
+        return $this->registrationDeadline;
+    }
+
+    public function setRegistrationDeadline(\DateTimeInterface $registrationDeadline): static
+    {
+        $this->registrationDeadline = $registrationDeadline;
+
+        return $this;
+    }
+
+    public function getNbRegistrationMax(): ?int
+    {
+        return $this->nbRegistrationMax;
+    }
+
+    public function setNbRegistrationMax(int $nbRegistrationMax): static
+    {
+        $this->nbRegistrationMax = $nbRegistrationMax;
+
+        return $this;
+    }
+
+    public function getInfoOuting(): ?string
+    {
+        return $this->infoOuting;
+    }
+
+    public function setInfoOuting(string $infoOuting): static
+    {
+        $this->infoOuting = $infoOuting;
+
+        return $this;
+    }
+
+}
