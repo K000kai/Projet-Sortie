889b7f995ca3512411b241ce6fab4852775db36c
K000kai <156654837+K000kai@users.noreply.github.com>
2024-03-13 15:05:34 +0100

    supression relation

---
+++ src/DataFixtures/CampusFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CampusFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $campusNantes = new Campus();
+        $campusNantes->setName('Nantes');
+
+        $manager->persist($campusNantes);
+        $manager->flush();
+    }
+}
