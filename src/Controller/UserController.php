3f72bb45d9ca09addd2de0c881644f9b82494988
StrifeyBoy (Eli) <elinkstrife@gmail.com>
2024-03-13 16:15:48 +0100

    Création fonction Register (à finir) + ajout bouton inscription

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Outing;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,4 +19,20 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+
+
+    #[Route('/user/register', name: 'app_user_register')]
+    public function register(Request $request, Outing $outing, User $user): Response
+    {
+        if ($outing->getUsers() !== $this->getUser()) {
+            echo "";
+            return $this->redirectToRoute('app_outing_index');
+        }
+
+        return $this->render('user/register.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
 }
